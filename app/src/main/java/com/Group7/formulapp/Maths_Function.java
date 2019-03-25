d3647eb5ea79cfd329f678ccee9f524096a6b293
Margaux <margaux.boulle@laposte.net>
2019-03-25 21:43:45 +0000

    Add the graph and a small activity example with the parser of equation The user can now : enter a fct, display it and see its graph

---
+++ app/src/main/java/com/Group7/formulapp/Maths_Function.java
@@ -0,0 +1,65 @@
+package com.Group7.formulapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.*;
+
+import org.nfunk.jep.*;
+
+
+
+
+import io.github.kexanie.library.MathView;
+
+
+public class Maths_Function extends AppCompatActivity {
+
+    private LineGraphSeries<DataPoint> series;
+    protected double result;
+    protected String fct;
+    MathView formula_two;
+   /* String tex = "This come from string. You can insert inline formula:" +
+            " $$cos^2$$ " + "test" +
+            "or displayed formula: $$\\sum_{i=0}^n i^2 = \\frac{(n^2+n)(2n+1)}{6}$$";*/
+
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_maths__function);
+            Button clickButton = (Button) findViewById(R.id.submit_fct);
+            clickButton.setOnClickListener( new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    formula_two = (MathView) findViewById(R.id.formula_two);
+                    EditText tex = (EditText) findViewById(R.id.equation);
+                    formula_two.setText("$$"+tex.getText().toString()+"$$");
+                     fct = tex.getText().toString();
+
+
+                }
+            });
+        }
+
+
+
+    //Action to go to the Graph view
+    protected void goToGraph(View view) {
+
+        Intent intent = new Intent(this, Maths_Graph.class);
+        intent.putExtra("fct", fct);
+
+        startActivity(intent);
+    }
+    }
+
+
+
+
---
+++ app/src/main/java/com/Group7/formulapp/Maths_Graph.java
@@ -0,0 +1,62 @@
+package com.Group7.formulapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.*;
+
+import org.nfunk.jep.JEP;
+
+public class Maths_Graph extends AppCompatActivity {
+
+    private LineGraphSeries<DataPoint> series;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+        Intent intent = getIntent();
+         String fct = intent.getStringExtra("fct");
+
+        JEP myParser = new JEP();
+        myParser.addStandardFunctions();
+        myParser.addStandardConstants();
+
+
+
+        double x,y;
+        x =0;
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series= new LineGraphSeries<>();
+
+        //Define the number of data point we use to build the graph
+        int numPoints = 500;
+        for(int i = 0; i <numPoints; i++){
+            x = x+0.1; //We increase the value of x of 0.1, to have a continue graph
+            myParser.addVariable("x", x);
+            myParser.parseExpression(fct);
+            y = myParser.getValue();
+
+            series.appendData(new DataPoint(x,y),true,100); // We add the point to the series
+        }
+
+        // set manual X bounds
+        /*graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(-100);
+        graph.getViewport().setMaxY(100);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(-100);
+        graph.getViewport().setMaxX(100);*/
+
+        // enable scaling and scrolling
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+
+        //Now we add the serie to the graph
+        graph.addSeries(series);
+
+    }
+}
