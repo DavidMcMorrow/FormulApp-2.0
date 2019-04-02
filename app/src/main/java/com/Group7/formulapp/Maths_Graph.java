71ecad37a77556d97c19fafb016ecc6e3b7b0cb7
Margaux <margaux.boulle@laposte.net>
2019-04-02 14:49:46 +0100

    Add the option of define boundarie for vertical axis of the graph

--- app/src/main/java/com/Group7/formulapp/Maths_Graph.java
+++ app/src/main/java/com/Group7/formulapp/Maths_Graph.java
@@ -14,11 +14,16 @@ import android.os.Bundle;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.*;
 import com.jjoe64.graphview.LegendRenderer;
+
 import org.nfunk.jep.JEP;
+
 import java.util.Random;
+
 import android.graphics.*;
 import android.view.*;
+
 import java.io.*;
+
 import android.net.*;
 
 
@@ -27,11 +32,15 @@ public class Maths_Graph extends AppCompatActivity {
     private LineGraphSeries<DataPoint> series;
     private String[] fctList;
     private String var;
-    private String lowerBound;
-    private String upperBound;
+    private String lowerBoundx;
+    private String upperBoundx;
+    private String lowerBoundy;
+    private String upperBoundy;
     private double x;
     private double minX;
     private double maxX;
+    private double minY;
+    private double maxY;
     private double y;
     private GraphView graph;
     public static final int STORAGE_PERMISSION_CODE = 1;
@@ -45,8 +54,10 @@ public class Maths_Graph extends AppCompatActivity {
         Intent intent = getIntent();
         fctList = intent.getStringArrayExtra("fct");
         var = intent.getStringExtra("variable");
-        lowerBound = intent.getStringExtra("lower");
-        upperBound = intent.getStringExtra("upper");
+        lowerBoundx = intent.getStringExtra("lowerx");
+        upperBoundx= intent.getStringExtra("upperx");
+        lowerBoundy = intent.getStringExtra("lowery");
+        upperBoundy= intent.getStringExtra("uppery");
 
         //We create the parser with a call to the library JEP
         JEP myParser = new JEP();
@@ -54,40 +65,44 @@ public class Maths_Graph extends AppCompatActivity {
         myParser.addStandardConstants();
 
         //Creation of a new Graph
-       graph = (GraphView) findViewById(R.id.graph);
+        graph = (GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<>();
 
         //Define the number of data point we use to build the graph
         int numPoints = 100000;
-        for (int j = 0; j < fctList.length -1; j++) {
+        for (int j = 0; j < fctList.length - 1; j++) {
 
-            if (fctList[j]!=null &&!fctList[j].isEmpty()) {
+            if (fctList[j] != null && !fctList[j].isEmpty()) {
 
 
                 // returns double primitive
-                minX = Double.parseDouble(lowerBound); //min x = lower bound define by user
-                maxX = Double.parseDouble(upperBound);
+                minX = Double.parseDouble(lowerBoundx); //min x = lower bound define by user for horizontal axis
+                maxX = Double.parseDouble(upperBoundx);
 
-                x=minX;
+                // returns double primitive
+                minY = Double.parseDouble(lowerBoundy); //min y = lower bound define by user for horizontal axis
+                maxY = Double.parseDouble(upperBoundy);
+
+                x = minX;
 
                 series = new LineGraphSeries<>();
 
                 for (int i = 0; i < numPoints; i++) {
-                    while(x<maxX){
-                    x = x + 0.1; //We increase the value of x of 0.1, to have a continue graph
-                    myParser.addVariable(var, x);
-                    //We parse each function(string) that the user entered
-                    myParser.parseExpression(fctList[j]);
-                    y = myParser.getValue();
-                    // We add the point to the series
-                    series.appendData(new DataPoint(x, y), true, 100000);
+                    while (x < maxX) {
+                        x = x + 0.1; //We increase the value of x of 0.1, to have a continue graph
+                        myParser.addVariable(var, x);
+                        //We parse each function(string) that the user entered
+                        myParser.parseExpression(fctList[j]);
+                        y = myParser.getValue();
+                        // We add the point to the series
+                        series.appendData(new DataPoint(x, y), true, 100000);
 
-                }
+                    }
                 }
                 //Now we add the series to the graph
                 graph.addSeries(series);
                 //Add a title to each function
-               series.setTitle(fctList[j]);
+                series.setTitle(fctList[j]);
                 graph.getLegendRenderer().setVisible(true);
                 graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
                 graph.getLegendRenderer().setBackgroundColor(Color.LTGRAY);
@@ -106,8 +121,8 @@ public class Maths_Graph extends AppCompatActivity {
         }
         // set manual X bounds
         graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setMinY(-100);
-        graph.getViewport().setMaxY(100);
+        graph.getViewport().setMinY(minY);
+        graph.getViewport().setMaxY(maxY);
 
         graph.getViewport().setXAxisBoundsManual(true);
         graph.getViewport().setMinX(minX);
@@ -118,37 +133,34 @@ public class Maths_Graph extends AppCompatActivity {
         graph.getViewport().setScalableY(true);
 
 
-
-
-
-
-
     }
 
     // Take a capture of the graph and share it
-    public void takeSnap(View view){
+    public void takeSnap(View view) {
 
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            // directly share it
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) { // Check the permission
 
 
             // get the bitmap
             Bitmap bitmap = graph.takeSnapshot();
 
             try {
-                File file = new File(this.getExternalCacheDir(),"myImage.png");
+                File file = new File(this.getExternalCacheDir(), "myImage.png");
                 FileOutputStream fOut = new FileOutputStream(file);
-                bitmap.compress(Bitmap.CompressFormat.PNG,80,fOut);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 80, fOut);
+
+                // Flushes the output stream and forces any buffered output bytes to be written out
                 fOut.flush();
                 fOut.close();
-                file.setReadable(true,false);
-                //Sharinf intent
+                file.setReadable(true, false);
+
+                //Sharing intent
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra(Intent.EXTRA_STREAM,Uri.fromFile(file));
+                intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
                 intent.setType("image/png");
-                startActivity(Intent.createChooser(intent,"Share Image via"));
+                startActivity(Intent.createChooser(intent, "Share Image via"));
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
 
@@ -157,14 +169,14 @@ public class Maths_Graph extends AppCompatActivity {
             }
 
 
-        } else {
+        } else { // if we don't have the permission we ask for it
             requestStoragePermission();
         }
 
 
     }
 
-    // Permission necessary for the API above 23
+    // Request for permission
     private void requestStoragePermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
@@ -176,7 +188,7 @@ public class Maths_Graph extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             ActivityCompat.requestPermissions(Maths_Graph.this,
-                                    new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
                         }
                     })
                     .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
@@ -189,7 +201,7 @@ public class Maths_Graph extends AppCompatActivity {
 
         } else {
             ActivityCompat.requestPermissions(this,
-                    new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
         }
     }
 }
--- app/src/main/java/com/Group7/formulapp/Maths_Setting_Graph.java
+++ app/src/main/java/com/Group7/formulapp/Maths_Setting_Graph.java
@@ -18,15 +18,24 @@ public class Maths_Setting_Graph extends AppCompatActivity implements AdapterVie
     private TextView parentText;
     private TextView upperText;
     private TextView lowerText;
-    private EditText lowerEdit;
-    private EditText upperEdit;
+    private EditText lowerEditx;
+    private EditText upperEditx;
+    private EditText lowerEdity;
+    private EditText upperEdity;
+    private String upperx;
+    private String lowerx;
+    private String uppery;
+    private String lowery;
+
     private TextView txt;
     private String[] fctList;
     private String var;
     private RelativeLayout mLayout;
-    private int previous=0;
-
-
+    private int previous = 0;
+    private Double dlowerx;
+    private Double dupperx;
+    private Double dlowery;
+    private Double duppery;
 
 
     @Override
@@ -37,12 +46,14 @@ public class Maths_Setting_Graph extends AppCompatActivity implements AdapterVie
         //initialisation
         okButton = (Button) findViewById(R.id.ok_btn);
         mLayout = (RelativeLayout) findViewById(R.id.relative_setting);
-        lowerEdit = (EditText) findViewById(R.id.lower_bound);
-        upperEdit = (EditText) findViewById(R.id.upper_bound);
+        lowerEditx = (EditText) findViewById(R.id.lower_bound_x);
+        upperEditx = (EditText) findViewById(R.id.upper_bound_x);
+        lowerEdity = (EditText) findViewById(R.id.lower_bound_y);
+        upperEdity = (EditText) findViewById(R.id.upper_bound_y);
         upperText = (TextView) findViewById(R.id.txt_upper_bound);
         lowerText = (TextView) findViewById(R.id.txt_lower_bound);
+        txt = (TextView) findViewById(R.id.txt);
 
-txt =(TextView) findViewById(R.id.txt);
 
         //We gather the information of the previous activity
         Intent intent = getIntent();
@@ -51,10 +62,10 @@ txt =(TextView) findViewById(R.id.txt);
 
         for (int i = 0; i < fctList.length; i++) {
             if (fctList[i] != null) {
-if(i!=0){
-                mLayout.addView(createNewTextView(i,fctList[i]));}
+                if (i != 0) {
+                    mLayout.addView(createNewTextView(i, fctList[i]));
+                }
                 mLayout.addView(createNewSpinner(i));
-
 
 
             }
@@ -72,32 +83,28 @@ if(i!=0){
 
 
         //We need to move the upper and lower boundaries display according to the creation of new textView
-       RelativeLayout.LayoutParams mLayout2 = (RelativeLayout.LayoutParams) lowerText.getLayoutParams();
+        RelativeLayout.LayoutParams mLayout2 = (RelativeLayout.LayoutParams) lowerText.getLayoutParams();
         mLayout2.addRule(RelativeLayout.BELOW, textView.getId());
         RelativeLayout.LayoutParams mLayout3 = (RelativeLayout.LayoutParams) upperText.getLayoutParams();
         mLayout3.addRule(RelativeLayout.BELOW, textView.getId());
 
 
-
         //We create a new relative layout to be able to display the new text view above the others
         RelativeLayout.LayoutParams p = new RelativeLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
 
-       if(i==1){
+        if (i == 1) {
 
             p.addRule(RelativeLayout.BELOW, txt.getId());
 
-        }
-
-        else {
+        } else {
 
             p.addRule(RelativeLayout.BELOW, parentText.getId());
 
 
-
         }
-        int numFct = i+1;
+        int numFct = i + 1;
 
-        textView.setText("Function" + i+ currentFct);
+        textView.setText("Function" + i + currentFct);
         parentText = textView;
         textView.setLayoutParams(p);
         return textView;
@@ -105,7 +112,7 @@ if(i!=0){
 
     }
 
-    protected Spinner createNewSpinner(int id){
+    protected Spinner createNewSpinner(int id) {
 
         Spinner spin = new Spinner(this);
 
@@ -120,31 +127,28 @@ if(i!=0){
         spin.setAdapter(dataAdapter);
         spin.setOnItemSelectedListener(this);
 
-        if(id==0){
+        if (id == 0) {
 
 
-            pbis.addRule(RelativeLayout.ALIGN_BASELINE,txt.getId());
-            pbis.addRule(RelativeLayout.ALIGN_PARENT_RIGHT,txt.getId());
-            previous =id;
+            pbis.addRule(RelativeLayout.ALIGN_BASELINE, txt.getId());
+            pbis.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, txt.getId());
+            previous = id;
 
 
         }
 
-        if(id==1){
-        pbis.addRule(RelativeLayout.BELOW, txt.getId());
-        previous =id;
+        if (id == 1) {
+            pbis.addRule(RelativeLayout.BELOW, txt.getId());
+            previous = id;
             pbis.addRule(RelativeLayout.RIGHT_OF, id);
 
-        }
-        else{
+        } else {
 
             pbis.addRule(RelativeLayout.BELOW, previous);
-            previous =id;
+            previous = id;
             pbis.addRule(RelativeLayout.RIGHT_OF, id);
 
-            }
-
-
+        }
 
 
         spin.setLayoutParams(pbis);
@@ -156,12 +160,18 @@ if(i!=0){
 
     //Action to go to the Graph view
     protected void goToGraph(View view) {
+        upperx = upperEditx.getText().toString();
+        lowerx = lowerEditx.getText().toString();
+        uppery = upperEdity.getText().toString();
+        lowery = lowerEdity.getText().toString();
 
         Intent intent = new Intent(this, Maths_Graph.class);
         intent.putExtra("fct", fctList);
         intent.putExtra("variable", var);
-        intent.putExtra("upper", upperEdit.getText().toString());
-        intent.putExtra("lower", lowerEdit.getText().toString());
+        intent.putExtra("lowerx", lowerx);
+        intent.putExtra("upperx", upperx);
+        intent.putExtra("lowery", lowery);
+        intent.putExtra("uppery", uppery);
 
         if (checkSetting(view, intent)) {
 
@@ -174,21 +184,43 @@ if(i!=0){
     protected boolean checkSetting(final View view, Intent intent) {
         //The user must provide the upper and the lower bound
 
-        if (lowerEdit.getText().toString().equals("")) {
-            lowerEdit.setError("Required!");
+        if (lowerEditx.getText().toString().equals("")) {
+            lowerEditx.setError("Required!");
             return false;
 
         }
 
-        if (upperEdit.getText().toString().equals("")) {
-            upperEdit.setError("Required!");
+        if (upperEditx.getText().toString().equals("")) {
+            upperEditx.setError("Required!");
             return false;
 
         }
+        //We need to check that upperboundarie is bigger than lowerboundarie otherwise its not going to work
+        dlowerx = Double.valueOf(lowerx);
+        dupperx = Double.valueOf(upperx);
+        dlowery = Double.valueOf(lowery);
+        duppery = Double.valueOf(uppery);
+
+        if (!upperValid(dlowerx, dupperx)) {
+            upperEditx.setError("Upper limit needs to be bigger than the lower limit");
+            return false;
+        }
+
+        if (!upperValid(dlowery, duppery)) {
+            upperEdity.setError("Upper limit needs to be bigger than the lower limit");
+            return false;
+        }
+
         return true;
     }
 
-//For the spinner
+
+    public boolean upperValid(double lower, double upper) {
+
+        return (lower < upper);
+    }
+
+    //For the spinner
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
